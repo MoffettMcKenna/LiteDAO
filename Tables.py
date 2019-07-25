bcaeeeac929dc76f6b5fe26bb2f9e09a87cdbd05
Moffett McKenna <moffett.mckenna@gmail.com>
2019-07-25 00:45:01 -0400

    Sync before chromebook service

--- Tables.py
+++ Tables.py
@@ -2,14 +2,18 @@ import sqlite3
 
 class Table
 
-    __select = "Select {} From {}"  # 1:column(s) 2:table(s)
-    __where  = "Where {} {} {}"
-    __ljoin  = "Left Join {} on {} = {}"
-
+    __SELECT = "Select {Columns} From {Table}\n"  # 1:column(s) 2:table(s)
+    __WHERE  = "Where {Column} {Operator} {Value}"
+    __WHEREA = " and {Column} {Operator} {Value}\n"
+    __LEFTJN = "Left Join {Table2} on {TableCol} = {Table2Col}\n"
+    __ORDER  = "Order By {Column}\n"
 
     def __init__(self, name, dbfile):
         self.DB = dbfile
         self.TableName = name
+
+        # initialize the select statement
+        self.__select = "Select {Columns} From " + self.TableName
 
         # grab the data
         c = sqlite3.connect(self.DB)
@@ -52,12 +56,44 @@ class Table
     #end Join()
 
 
-    def Get(self, where = None, columns)
+    def Get(self, columns, where = None)
         '''
         Retrieves all values of a set of columns.  If the where clause is specified then only the matching values are returned.
-        Params:
-            columns - 
+        
+        Args:
+            columns: A list of the columns to select.
+            where: A list of tuples, each tuple being a set of column name, comparison operator, and value.
+        Return:
+            
         '''
+
+        # turn the columns into a comma seperated list
+        cols = ', '.join(columns)
+        # TODO scrub the columns before the join
+
+        # start the query
+        query = self.__select.format_map({'Columns':cols})
+
+        wpresent = False
+
+        #handle adding the where clause, if there is one
+        if where != None:
+            for w in where:
+                # TODO scrub the items in the tuple - also sanity check the operator
+                if wpresent:
+                    # "Where {Column} {Operator} {Value}"
+                    query += __WHERE.format_map('Column':w[0], 'Operator':w[1], 'Value':w[2])  
+                    wpresent = True
+                else:
+                    # " and {Column} {Operator} {Value}\n"
+                    query += __WHEREA.format_map('Column':w[0], 'Operator':w[1], 'Value':w[2])  
+            # end for where
+        # end if where
+
+        # run the query
+        c = sqlite3.connect(self.DB)
+        cols = c.execute(query).fetchall()
+
         return
     #end Get()
 
@@ -66,3 +102,10 @@ class Table
 
 if __name__ == "__main__":
     
+    # simple get
+
+    # one record get
+
+    # get with where
+
+    # get with compound where clause
